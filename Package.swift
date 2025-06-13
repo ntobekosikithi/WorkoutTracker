904ca9d35850a7597451264c91828d580a8eff6d
Ntobeko Sikithi <ntobekosikithi@gmail.com.com>
2025-06-13 20:25:13 +0200

    Initial commit

--- Package.swift
+++ Package.swift
@@ -11,11 +11,15 @@ let package = Package(
             name: "WorkoutTracker",
             targets: ["WorkoutTracker"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/ntobekosikithi/Utilities.git", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WorkoutTracker"),
+            name: "WorkoutTracker",
+            dependencies: ["Utilities"]),
         .testTarget(
             name: "WorkoutTrackerTests",
             dependencies: ["WorkoutTracker"]
